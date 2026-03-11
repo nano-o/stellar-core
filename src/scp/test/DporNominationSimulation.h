18611ecac10312986d3769adba5085e27bfb03a1
Giuliano Losa <giuliano@losa.fr>
2026-03-11 21:15:29 +0000

    Add SCP nomination simulation harness

---
+++ src/scp/test/DporNominationSimulation.h
@@ -0,0 +1,152 @@
+// Copyright 2024 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#pragma once
+
+#include "crypto/SecretKey.h"
+#include "scp/SCP.h"
+
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <map>
+#include <memory>
+#include <optional>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace stellar
+{
+
+class DporNominationNode : public SCPDriver
+{
+  public:
+    struct TimerState
+    {
+        uint64 mSlotIndex;
+        int mTimerID;
+        std::chrono::milliseconds mTimeout;
+        std::function<void()> mCallback;
+    };
+
+    explicit DporNominationNode(SecretKey const& secretKey,
+                                SCPQuorumSet const& localQSet);
+
+    NodeID const&
+    getNodeID() const;
+
+    SCP&
+    getSCP();
+
+    SCP const&
+    getSCP() const;
+
+    void storeQuorumSet(SCPQuorumSet const& qSet);
+
+    bool nominate(uint64 slotIndex, Value const& value,
+                  Value const& previousValue);
+
+    SCP::EnvelopeState receiveEnvelope(SCPEnvelope const& envelope);
+
+    void setStateFromEnvelope(uint64 slotIndex, SCPEnvelope const& envelope);
+
+    std::vector<SCPEnvelope> takePendingEnvelopes();
+
+    std::vector<SCPEnvelope> const&
+    getEmittedEnvelopes() const;
+
+    std::vector<SCPEnvelope> getLatestMessagesSend(uint64 slotIndex);
+
+    std::set<NodeID> getNominationLeaders(uint64 slotIndex);
+
+    void setPriorityLookup(std::function<uint64(NodeID const&)> const& fn);
+
+    void setValueHash(std::function<uint64(Value const&)> const& fn);
+
+    void setCombineCandidates(
+        std::function<ValueWrapperPtr(uint64, ValueWrapperPtrSet const&)> const&
+            fn);
+
+    bool hasActiveTimer(uint64 slotIndex, int timerID) const;
+
+    std::optional<TimerState> getTimer(uint64 slotIndex, int timerID) const;
+
+    bool fireTimer(uint64 slotIndex, int timerID);
+
+    void signEnvelope(SCPEnvelope& envelope) override;
+    SCPQuorumSetPtr getQSet(Hash const& qSetHash) override;
+    void emitEnvelope(SCPEnvelope const& envelope) override;
+    ValidationLevel validateValue(uint64 slotIndex, Value const& value,
+                                  bool nomination) override;
+    Hash getHashOf(std::vector<xdr::opaque_vec<>> const& vals) const override;
+    uint64 computeValueHash(uint64 slotIndex, Value const& prev,
+                            int32_t roundNumber, Value const& value) override;
+    ValueWrapperPtr combineCandidates(
+        uint64 slotIndex, ValueWrapperPtrSet const& candidates) override;
+    bool hasUpgrades(Value const& value) override;
+    ValueWrapperPtr stripAllUpgrades(Value const& value) override;
+    uint32_t getUpgradeNominationTimeoutLimit() const override;
+    void setupTimer(uint64 slotIndex, int timerID,
+                    std::chrono::milliseconds timeout,
+                    std::function<void()> cb) override;
+    void stopTimer(uint64 slotIndex, int timerID) override;
+    std::chrono::milliseconds computeTimeout(uint32 roundNumber,
+                                             bool isNomination) override;
+
+  private:
+    using TimerKey = std::pair<uint64, int>;
+
+    SecretKey mSecretKey;
+    SCP mSCP;
+    std::map<Hash, SCPQuorumSetPtr> mQuorumSets;
+    std::vector<SCPEnvelope> mEmittedEnvelopes;
+    std::vector<SCPEnvelope> mPendingEnvelopes;
+    std::map<TimerKey, TimerState> mTimers;
+    std::function<uint64(Value const&)> mValueHash;
+    std::function<ValueWrapperPtr(uint64, ValueWrapperPtrSet const&)>
+        mCombineCandidates;
+};
+
+class DporNominationSimulation
+{
+  public:
+    explicit DporNominationSimulation(std::vector<SecretKey> const& validators,
+                                      SCPQuorumSet const& qSet);
+
+    static std::vector<SecretKey>
+    makeValidatorSecretKeys(std::string const& seedPrefix, std::size_t count);
+
+    static std::vector<NodeID>
+    getNodeIDs(std::vector<SecretKey> const& validators);
+
+    static SCPQuorumSet makeQuorumSet(std::vector<NodeID> const& nodeIDs,
+                                      uint32_t threshold);
+
+    std::size_t
+    size() const;
+
+    DporNominationNode&
+    getNode(std::size_t index);
+
+    DporNominationNode const&
+    getNode(std::size_t index) const;
+
+    void setPriorityLookup(std::function<uint64(NodeID const&)> const& fn);
+
+    void setValueHash(std::function<uint64(Value const&)> const& fn);
+
+    void setCombineCandidates(
+        std::function<ValueWrapperPtr(uint64, ValueWrapperPtrSet const&)> const&
+            fn);
+
+    std::size_t broadcastPendingEnvelopesOnce();
+
+  private:
+    std::vector<std::unique_ptr<DporNominationNode>> mNodes;
+};
+
+}
---
+++ src/scp/test/SCPDporNominationTests.cpp
@@ -0,0 +1,133 @@
+// Copyright 2024 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "scp/test/DporNominationSimulation.h"
+
+#include "crypto/SHA.h"
+#include "scp/Slot.h"
+#include "test/Catch2.h"
+#include "xdrpp/marshal.h"
+
+#include <algorithm>
+
+namespace stellar
+{
+
+namespace
+{
+
+Value
+makeValue(std::string const& label)
+{
+    return xdr::xdr_to_opaque(sha256(label));
+}
+
+void
+requireNominationEnvelope(SCPEnvelope const& envelope, NodeID const& nodeID,
+                          Hash const& qSetHash, uint64 slotIndex,
+                          std::vector<Value> votes,
+                          std::vector<Value> accepted)
+{
+    std::sort(votes.begin(), votes.end());
+    std::sort(accepted.begin(), accepted.end());
+
+    REQUIRE(envelope.statement.nodeID == nodeID);
+    REQUIRE(envelope.statement.slotIndex == slotIndex);
+    REQUIRE(envelope.statement.pledges.type() == SCP_ST_NOMINATE);
+
+    auto const& nomination = envelope.statement.pledges.nominate();
+    REQUIRE(nomination.quorumSetHash == qSetHash);
+    REQUIRE(std::vector<Value>(nomination.votes.begin(), nomination.votes.end()) ==
+            votes);
+    REQUIRE(std::vector<Value>(nomination.accepted.begin(),
+                               nomination.accepted.end()) == accepted);
+}
+
+void
+requirePrepareEnvelope(SCPEnvelope const& envelope, NodeID const& nodeID,
+                       Hash const& qSetHash, uint64 slotIndex,
+                       SCPBallot const& ballot)
+{
+    REQUIRE(envelope.statement.nodeID == nodeID);
+    REQUIRE(envelope.statement.slotIndex == slotIndex);
+    REQUIRE(envelope.statement.pledges.type() == SCP_ST_PREPARE);
+
+    auto const& prepare = envelope.statement.pledges.prepare();
+    REQUIRE(prepare.quorumSetHash == qSetHash);
+    REQUIRE(prepare.ballot == ballot);
+}
+
+}
+
+TEST_CASE("dpor nomination harness reproduces a simple leader scenario",
+          "[scp][dpor][nomination]")
+{
+    auto validators =
+        DporNominationSimulation::makeValidatorSecretKeys("dpor-nomination-", 4);
+    auto nodeIDs = DporNominationSimulation::getNodeIDs(validators);
+    auto qSet = DporNominationSimulation::makeQuorumSet(nodeIDs, 3);
+
+    auto previousValue = makeValue("previous");
+    auto xValue = makeValue("x");
+    auto yValue = makeValue("y");
+
+    DporNominationSimulation simulation(validators, qSet);
+    simulation.setPriorityLookup([&](NodeID const& nodeID) {
+        return nodeID == nodeIDs[0] ? UINT64_MAX : 1;
+    });
+
+    auto localQSetHash = [&](std::size_t index) {
+        return simulation.getNode(index).getSCP().getLocalNode()->getQuorumSetHash();
+    };
+
+    REQUIRE(simulation.size() == 4);
+
+    REQUIRE(simulation.getNode(0).nominate(0, xValue, previousValue));
+    REQUIRE_FALSE(simulation.getNode(1).nominate(0, yValue, previousValue));
+    REQUIRE_FALSE(simulation.getNode(2).nominate(0, yValue, previousValue));
+    REQUIRE_FALSE(simulation.getNode(3).nominate(0, yValue, previousValue));
+
+    REQUIRE(simulation.getNode(1).hasActiveTimer(0, Slot::NOMINATION_TIMER));
+    REQUIRE(simulation.getNode(2).hasActiveTimer(0, Slot::NOMINATION_TIMER));
+    REQUIRE(simulation.getNode(3).hasActiveTimer(0, Slot::NOMINATION_TIMER));
+
+    auto const& leaderEnvelopes = simulation.getNode(0).getEmittedEnvelopes();
+    REQUIRE(leaderEnvelopes.size() == 1);
+    requireNominationEnvelope(leaderEnvelopes[0], nodeIDs[0], localQSetHash(0), 0,
+                              {xValue}, {});
+
+    REQUIRE(simulation.broadcastPendingEnvelopesOnce() == 3);
+
+    for (std::size_t i = 1; i < simulation.size(); ++i)
+    {
+        auto const& peerEnvelopes = simulation.getNode(i).getEmittedEnvelopes();
+        REQUIRE(peerEnvelopes.size() == 1);
+        requireNominationEnvelope(peerEnvelopes[0], nodeIDs[i], localQSetHash(i), 0,
+                                  {xValue}, {});
+    }
+
+    REQUIRE(simulation.broadcastPendingEnvelopesOnce() == 9);
+
+    auto const& leaderUpdates = simulation.getNode(0).getEmittedEnvelopes();
+    REQUIRE(leaderUpdates.size() >= 2);
+    requireNominationEnvelope(leaderUpdates[1], nodeIDs[0], localQSetHash(0), 0,
+                              {xValue}, {xValue});
+
+    REQUIRE(simulation.broadcastPendingEnvelopesOnce() > 0);
+
+    auto const& finalLeaderEnvelopes = simulation.getNode(0).getEmittedEnvelopes();
+    auto prepareIt = std::find_if(finalLeaderEnvelopes.begin(),
+                                  finalLeaderEnvelopes.end(),
+                                  [](SCPEnvelope const& envelope) {
+                                      return envelope.statement.pledges.type() ==
+                                             SCP_ST_PREPARE;
+                                  });
+    REQUIRE(prepareIt != finalLeaderEnvelopes.end());
+    requirePrepareEnvelope(*prepareIt, nodeIDs[0], localQSetHash(0), 0,
+                           SCPBallot{1, xValue});
+    REQUIRE_FALSE(
+        simulation.getNode(0).hasActiveTimer(0, Slot::NOMINATION_TIMER));
+}
+
+}
